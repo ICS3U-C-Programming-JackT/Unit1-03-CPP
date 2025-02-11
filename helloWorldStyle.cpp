dfe692e553eefc28041084321f96a8169f52f1b3
Jack Turcotte <jack.turcotte@stu.ocsb.ca>
2025-02-11 17:55:51 +0000

    Finished

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2025 Jack Turcotte All rights reserved.
+//
+// Created by: Jack Turcotte
+// Date: February 11, 2025
+// This program displays "Hello world" with proper style
+
+#include <iostream>
+
+int main() {
+    std::string msg_1 = "Hello, World! (With style!)";
+    std::string msg_2 = "C++ is EASY";
+
+    std::cout << msg_1 + msg_2 << std::endl;
+}
